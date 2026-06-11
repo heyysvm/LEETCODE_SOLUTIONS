d5ef0bb2d490de27ee8e16a7cefadf84bbdc231c
Shivam <imshivam339@gmail.com>
2026-06-11 23:07:09 +0530

    Time: 0 ms (100%), Space: 9 MB (8.65%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size()-1;
+        int c =0;
+        while(i>=0 && s[i]==' ')i--;
+        while(i>=0 && s[i]!=' '){
+            i--;
+            c++;
+        }
+        return c;
+    }
+};
