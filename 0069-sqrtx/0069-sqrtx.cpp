ad4231671fb726780be67b89b786f69656b9ad45
Shivam <imshivam339@gmail.com>
2026-06-11 23:17:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.86%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,10 +5,7 @@ public:
         while(l <= r){
             long long mid = l+(r-l)/2;
             if(mid * mid == x)return mid;
-            if(mid * mid < x){
-                l = mid+1;
-                ans = mid;
-            }
+            if(mid * mid < x)l = mid+1 ,ans = mid;
             else r=mid-1;
         }return ans;
     }
