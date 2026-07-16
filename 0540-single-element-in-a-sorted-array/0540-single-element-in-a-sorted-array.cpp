a5a7d49d7b2d3be0a845350f1bf0cbfcb4e1df64
Shivam <imshivam339@gmail.com>
2026-07-16 20:39:57 +0530

    Time: 0 ms (100%), Space: 26.3 MB (35.2%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -16,21 +16,21 @@ public:
         // }
         // return ans;
 
-    int n = nums.size()-1;
-    if(nums.size()==1) return nums[0];
-    if(nums[0]!=nums[1])return nums[0];
-    if(nums[n]!=nums[n-1])return nums[n];
+        int n = nums.size();
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
 
-    int low = 1, high = n-1;
-    while(low<=high){
-        int mid = (low+high)/2;
+        int low =1 , high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
 
-        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
-        if(mid%2==0 && nums[mid]==nums[mid+1] ||
-           mid%2!=0 && nums[mid]==nums[mid-1] ) low = mid+1;
-        else  high = mid-1;
-    }
-    return -1;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+            if(mid%2==0 && nums[mid]==nums[mid+1] ||
+               mid%2!=0 && nums[mid]==nums[mid-1]) low= mid+1;
+            else high = mid-1;
+        }
+        return -1;
 
     }
 };
