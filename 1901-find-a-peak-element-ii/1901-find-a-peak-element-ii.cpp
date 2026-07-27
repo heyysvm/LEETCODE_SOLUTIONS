b49c4517025baea92dc4bf05a651cec834df3896
Shivam <imshivam339@gmail.com>
2026-07-27 18:28:03 +0530

    Time: 0 ms (100%), Space: 50.3 MB (32.41%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,37 +1,51 @@
 class Solution {
 public:
 
-    int maxElement(vector<vector<int>>& mat , int col){
+    // int maxElement(vector<vector<int>>& mat , int col){
+    //     int n = mat.size();
+    //     int maxs = INT_MIN;
+    //     int idx = -1;
+    //     for(int i=0;i<n;i++){
+    //         if(mat[i][col]>maxs){
+    //             maxs = mat[i][col];
+    //             idx = i;
+    //         }
+    //     }
+    //     return idx;
+    // }
+
+    int maxEle(vector<vector<int>>& mat , int col){
         int n = mat.size();
+        int idx=-1;
         int maxs = INT_MIN;
-        int idx = -1;
+
         for(int i=0;i<n;i++){
             if(mat[i][col]>maxs){
                 maxs = mat[i][col];
-                idx = i;
+                idx =i;
             }
         }
         return idx;
     }
 
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+       int n = mat.size();
+       int m = mat[0].size();
 
-        int low = 0;
-        int high = m-1;
+       int low = 0;
+       int high = m-1;
 
-        while(low<=high){
-            int mid = (low+high)/2;
-            int row = maxElement(mat,mid);
+       while(low<=high){
+            int mid = low+(high-low)/2;
+            int row = maxEle(mat,mid);
 
-            int left = mid-1 >= 0 ? mat[row][mid-1] : INT_MIN;
+            int left =  mid-1 >= 0 ? mat[row][mid-1] : INT_MIN;
             int right = mid+1 < m ? mat[row][mid+1] : INT_MIN;
 
-            if(mat[row][mid]>left && mat[row][mid]>right) return{row,mid};
-            else if (right > mat[row][mid])low = mid+1;
-            else high = mid-1;
-        }
-        return {-1,-1};
+            if(mat[row][mid] > left && mat[row][mid] > right) return {row,mid};
+            else if (left > mat[row][mid]) high = mid-1;
+            else low = mid+1;
+       }
+       return {-1,-1};
     }
 };
