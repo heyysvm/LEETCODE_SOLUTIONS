cbfe051f8c4b01ac6004204521b36af99a83aea7
Shivam <imshivam339@gmail.com>
2026-08-19 16:12:10 +0530

    Time: 22 ms (59.73%), Space: 17.5 MB (56.67%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -42,27 +42,27 @@ public:
         // return vector<vector<int>>(st.begin(),st.end());
 
         int n = nums.size();
-        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(i>0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
                 if(j>i+1 && nums[j]==nums[j-1]) continue;
-                
-                int l = j+1 , r= n-1;
+                int l = j+1 , r = n-1;
                 while(l<r){
-                    long long sum = 1LL * nums[i] + nums[j] + nums[l] + nums[r];
+                    long long sum = 1LL * nums[j] + nums[i] + nums[l] + nums[r];
                     if(sum==target){
-                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        l++ , r--;
-                        while(l<r && nums[l]==nums[l-1])l++;
-                        while(l<r && nums[r]==nums[r+1])r--;
+                         ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                         l++ , r--; 
+                         while(l<r && nums[l]==nums[l-1]) l++;
+                         while(l<r && nums[r]==nums[r+1]) r--;
                     }
                     else if(sum<target) l++;
                     else r--;
+                    
                 }
             }
-        }
-    return ans;
+        } return ans;
     }
 };
